b85c097242ce773783b612706a81daaebc7e7eac
notEpsilon <ibrahimnasr393@gmail.com>
2020-03-31 10:44:31 +0200

    Updates

---
+++ precache-manifest.15a03ff960fa99de1fc75d3e08fa2d7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b958b52c6c39f05348814b7491f8233a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "29a475279e1091cbc89a",
+    "url": "/monsters-rolodex/static/css/main.be063c51.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a475279e1091cbc89a",
+    "url": "/monsters-rolodex/static/js/main.246ebf8b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
